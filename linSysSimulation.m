75086af9c355f3ff51ab8a823efbb743576dfc52
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-09-03 23:18:12 -0300

    add discrete time example

--- linSysSimulation.m
+++ linSysSimulation.m
@@ -26,3 +26,42 @@ grid
 ylabel('x(t) e y(t)')
 xlabel('t(s)')
 axis([0 5 -1.2 1.2])
+
+% determina ganho e fase diretamente da resposta em frequencia
+j=sqrt(-1);
+
+disp('ganho na frequencia w0')
+KH=abs(1/(j*w0+1))
+disp('fase em radianos na frequencia w0')
+phaseH=phase(1/(j*w0+1))
+
+%%
+% Ex2 tempo discreto
+% y[n]-(3/4)y[n-1]+(1/8)y[n-2]=2x[n]
+% entrada senoidal
+k=0:1000;
+w0=pi/10;
+x=cos(w0*k);
+
+% simulacao com condicoes iniciais nulas
+y(1)=0;
+y(2)=0;
+for n=3:length(k)
+    y(n)=(3/4)*y(n-1)-(1/8)*y(n-2)+2*x(n);
+end
+
+% grafico (regime transitorio)
+figure(3)
+set(gca,'FontSize',18)
+stem(k(1:50),x(1:50),'b');
+hold on
+stem(k(1:50),y(1:50),'r');
+hold off
+
+% determina ganho e fase diretamente da resposta em frequencia
+j=sqrt(-1);
+
+disp('ganho na frequencia w0')
+KHd=abs(2/(1-(3/4)*exp(-j*1*w0)+(1/8)*exp(-j*2*w0)))
+disp('fase em radianos na frequencia w0')
+phaseH=phase(2/(1-(3/4)*exp(-j*1*w0)+(1/8)*exp(-j*2*w0)))
