8fd458146090beab08d1976f54ba6af2933f131f
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-09-07 23:21:28 -0300

    add 9.20 ex

--- causalidade.m
+++ causalidade.m
@@ -1,3 +1,27 @@
+% Ex.9.20
+h1=exp(-t)*2/3+exp(2*t)/3;
+figure(1);
+plot(t,h1)
+axis([0 1 0 2])
+xlabel('t')
+ylabel('h')
+
+h2a=exp(-t)*2/3;
+h2b=-exp(2*(-t))/3;
+figure(2)
+plot(t,h2a,'b',-t,h2b,'b')
+xlabel('t')
+ylabel('h')
+
+h3=-(exp(-t)*2/3+exp(2*t)/3);
+figure(3);
+plot(-t,h3,'b')
+axis([-1 0 -2 0])
+xlabel('t')
+ylabel('h')
+
+
+%%
 % definicao degrau unitario 11 elementos
 % corresponde ao tempo n=0 a n=10
 u=ones(1,11);
